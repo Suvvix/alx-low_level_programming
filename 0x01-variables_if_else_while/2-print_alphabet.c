da61b988ae6e60bb8366e0947ee2f6c6384b4ecc
Suvvix <suvvix@gmail.com>
2023-10-08 13:50:35 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: alphabet letters.
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char cha = 'a';
+
+	while (cha <= 'z')
+	{
+		putchar(cha);
+		cha++
+	}
+	putchar('\n');
+
+	return (0);
+}
