93ab29d987b8e704fec238d3cd3ced59e96863b4
Suvvix <suvvix@gmail.com>
2023-10-08 15:20:33 -0700

    (

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: print 0-9 using putchar.
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
