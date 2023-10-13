ea7cb3df9eb3b5cb70e67a4a508542a34448bb52
Suvvix <suvvix@gmail.com>
2023-10-12 17:29:51 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,20 +12,22 @@ int main(void)
 {
 	int num;
 
-	for (num = 1; num <= 100; num++;)
+	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
 		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
-		else if (num % 3 == 0 && !(num % 5 == 0))
+		else if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz");
 		else
 			printf("%d", num);
+
 		if (num != 100)
 			printf(" ");
 		else
 			printf("\n");
-
 	}
+
+	return (0);
 }
