12602fd8c219c8cef4200b6d913da9faf06b8e3f
Suvvix <suvvix@gmail.com>
2023-10-12 17:06:33 -0700

    go

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- * print_square - prints a square, followed by a new line;
+ * print_square - prints a square, followed by a new line.
  * @size: size of the square
+ *
+ * Description: This function prints a square of a given size using the
+ * character '#'. If the size is 0 or less, it prints only a new line.
  */
 void print_square(int size)
 {
