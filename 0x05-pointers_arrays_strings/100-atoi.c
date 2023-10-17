88ba271fc6e8391279eba9ebf2bcd43670f75ca8
Suvvix <suvvix@gmail.com>
2023-10-16 19:07:08 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,32 +10,42 @@ int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
+	int seenDigit = 0;
 
-	while (*s == ' ' || (*s >= 9 && *s <= 13))
-		s++;
-
-	if (*s == '-')
+	while (*s)
 	{
-		sign = -1;
-		s++;
-	}
-	else if (*s == '+')
-	{
-		s++;
-	}
-
-	while (*s >= '0' && *s <= '9')
-	{
-		if (result > (INT_MAX - (*s - '0')) / 10)
+		if (*s == ' ')
 		{
-			if (sign == 1)
-				return (INT_MAX);
-			else
-				return (INT_MIN);
+			if (seenDigit)
+				break;
 		}
-		result = result * 10 + (*s - '0');
+		else if (*s == '-')
+		{
+			sign = -1;
+			seenDigit = 1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			seenDigit = 1;
+
+			if (result > (INT_MAX - (*s - '0')) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+
+			result = result * 10 + (*s - '0');
+		}
+		else if (seenDigit)
+		{
+			break;
+		}
+
 		s++;
 	}
 
 	return (sign * result);
 }
+
