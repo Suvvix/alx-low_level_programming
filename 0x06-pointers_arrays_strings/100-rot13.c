65eda9d3fe9b90cf4c83148b900d6256c8830c67
Suvvix <suvvix@gmail.com>
2023-10-19 10:41:45 -0700

    r

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,23 +10,23 @@
 
 char *rot13(char *s)
 {
-	int i;
+    int i;
+    char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+    char *ptr = s;
 
-	char rot13[] = {abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ}
-	char ROT13[] = {nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM}
-	char *ptr = s;
-
-	while (*s)
-	{
-		for (i = 0; i <= 52; i++)
-		{
-			if (*s == rot13[i])
-			{
-				*s = ROT13[i];
-				break;
-			}
-		}
-		s++;
-	}
-	return (ptr);
+    while (*s)
+    {
+        for (i = 0; i <= 52; i++)
+        {
+            if (*s == rot13[i])
+            {
+                *s = ROT13[i];
+                break;
+            }
+        }
+        s++;
+    }
+    return (ptr);
 }
+
