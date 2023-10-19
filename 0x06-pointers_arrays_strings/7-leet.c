5f38ff94b8afb68321be06e7f91824a83357d1b9
Suvvix <suvvix@gmail.com>
2023-10-19 10:28:51 -0700

    c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	char *cp = n;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*n == key[i] || *n == key[i] + 32)
+			{
+				*n = 48 + value[i];
+			}
+		}
+		c++;
+	}
+	return (cp);
+}
