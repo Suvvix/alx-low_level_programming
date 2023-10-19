902f817f94e89339b1061d95ec46715a6bffb6cd
Suvvix <suvvix@gmail.com>
2023-10-19 09:40:16 -0700

    toup

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int r;
+
+	for (r = 0; str[r] != '\0'; i++)
+	{
+		if (str[r] >= 97 && str[r] <= 122)
+		{
+			str[r] = str[r] - 32;
+		}
+	}
+	return (str);
+}
