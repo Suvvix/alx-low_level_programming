df1b319378606d7d2b50770b26982fe2ac1d7e73
Suvvix <suvvix@gmail.com>
2023-10-27 11:44:28 -0700

    done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguments passed
+ * @argc: number of commandline arg
+ * @argv: pointer to array of commandline arg
+ * Return: 0 - success, non-zero - fail.
+*/
