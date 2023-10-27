44beca157cd04e477c1b528931ed5a167ed882ef
Suvvix <suvvix@gmail.com>
2023-10-27 11:41:03 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of 2 numbers.
+ * @argc: number of commandline arg.
+ * @argv: pointer to array of commandline arg.
+ * Return: 0 - success, non-zero - fail.
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
