f2fb8be9338806f709d4022884618a88b3b17d4a
Suvvix <suvvix@gmail.com>
2023-10-31 10:44:41 -0700

    go

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,12 +9,16 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int len_s1, len_s2, i, j;
+	char *concatenated;
+
+	len_s1 = 0;
+	len_s2 = 0;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	int len_s1 = 0, len_s2 = 0;
 
 	while (s1[len_s1] != '\0')
 		len_s1++;
@@ -22,18 +26,21 @@ char *str_concat(char *s1, char *s2)
 	while (s2[len_s2] != '\0')
 		len_s2++;
 
-	char *concatenated = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	concatenated = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 
 	if (concatenated == NULL)
 		return (concatenated);
 
-	for (int i = 0; i < len_s1; i++)
+	for (i = 0; i < len_s1; i++)
 		concatenated[i] = s1[i];
 
-	for (int i = 0; i < len_s2; i++)
-		concatenated[len_s1 + i] = s2[i];
+	for (j = 0; j < len_s2; j++)
+	{
+		concatenated[i] = s2[j];
+		i++;
+	}
 
-	concatenated[len_s1 + len_s2] = '\0';
+	concatenated[i] = '\0';
 
 	return (concatenated);
 }
