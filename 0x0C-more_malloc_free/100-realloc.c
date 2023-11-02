38616664bfa726ac313b7bfce24785d51ae674f5
Suvvix <suvvix@gmail.com>
2023-11-02 10:19:26 -0700

    2

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _realloc - Reallocates memory using malloc and free
+*
+* @ptr: Pointer to previously allocated memory
+* @old_size: Size in bytes of the old allocated space for ptr
+* @new_size: The new size in bytes of the memory block
+*
+* Return: A pointer to the newly allocated memory, or NULL
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *Magnus;
+	unsigned int Paracelsus_size, Agrippa_size = new_size;
+	char *Paracelsus = ptr;
+
+	if (ptr == NULL)
+	{
+		Magnus = malloc(new_size);
+		return (Magnus);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	Magnus = malloc(new_size);
+	if (Magnus == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		Paracelsus_size = old_size;
+	for (unsigned int i = 0; i < Paracelsus_size; i++)
+		Magnus[i] = Paracelsus[i];
+	free(ptr);
+	return (Magnus);
+}
+
