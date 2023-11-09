8d91499f0c99870ea55a07dbc5d8285288ab661b
Suvvix <suvvix@gmail.com>
2023-11-09 14:19:40 -0800

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,46 +6,47 @@
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
  */
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
-    int index = 0;
-    char *current_str;
-    char *separator = "";
+	int index = 0;
+	char *current_str;
+	char *separator = "";
 
-    va_list arg_list;
+	va_list arg_list;
 
-    va_start(arg_list, format);
+	va_start(arg_list, format);
 
-    if (format != NULL)
-    {
-        while (format[index] != '\0')
-        {
-            switch (format[index])
-            {
-                case 'c':
-                    printf("%s%c", separator, va_arg(arg_list, int));
-                    break;
-                case 'i':
-                    printf("%s%d", separator, va_arg(arg_list, int));
-                    break;
-                case 'f':
-                    printf("%s%f", separator, va_arg(arg_list, double));
-                    break;
-                case 's':
-                    current_str = va_arg(arg_list, char *);
-                    if (current_str == NULL)
-                        current_str = "(nil)";
-                    printf("%s%s", separator, current_str);
-                    break;
-                default:
-                    index++;
-                    continue;
-            }
-            separator = ", ";
-            index++;
-        }
-    }
+	if (format != NULL)
+	{
+		while (format[index] != '\0')
+		{
+			switch (format[index])
+			{
+			case 'c':
+				printf("%s%c", separator, va_arg(arg_list, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(arg_list, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(arg_list, double));
+				break;
+			case 's':
+				current_str = va_arg(arg_list, char *);
+				if (current_str == NULL)
+					current_str = "(nil)";
+				printf("%s%s", separator, current_str);
+				break;
+			default:
+				index++;
+				continue;
+			}
+			separator = ", ";
+			index++;
+		}
+	}
 
-    printf("\n");
-    va_end(arg_list);
+	printf("\n");
+	va_end(arg_list);
 }
+
