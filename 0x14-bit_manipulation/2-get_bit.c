4d786d58475196eb4ca9fd1e6c3d72fdcc43abe2
Suvvix <suvvix@gmail.com>
2023-11-23 19:00:10 -0800

    ''

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit
+ * @n: the num to index
+ * @index: the bit
+ *
+ * Return: the bit state or -1 on error
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return (n >> index & 1);
+}
