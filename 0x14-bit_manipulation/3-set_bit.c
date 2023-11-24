d5c4da424b00c974dc3b5d7ca707abe97a3a4bcf
Suvvix <suvvix@gmail.com>
2023-11-23 19:03:46 -0800

    3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the bit
+ * @n: the numr to index
+ * @index: the bit
+ *
+ * Return: 1 if succesfful, -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index);
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return (!!(*n |= 1l << index));
+}
