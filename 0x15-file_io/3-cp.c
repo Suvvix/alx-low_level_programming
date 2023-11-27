4da940001183a9bce7fd5e1f283369c41df9ee5c
Suvvix <suvvix@gmail.com>
2023-11-27 10:44:25 -0800

    @

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,131 +1,74 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#define BUF_SIZE 1024
 
 /**
- * check_arguments - Check the number of command-line arguments.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
- *
- * Return: 1 if the number of arguments is incorrect, otherwise 0.
+ * check_files - verifies if files can be accessed.
+ * @source_fd: source file descriptor.
+ * @dest_fd: destination file descriptor.
+ * @args: command line arguments.
+ * Return: No return value.
  */
-int check_arguments(int argc, char *argv[])
+void check_files(int source_fd, int dest_fd, char *args[])
 {
-	if (argc != 3)
+	if (source_fd == -1)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		return (1);
+		custom_printf("Error: Can't read from file %s\n", args[1]);
+		exit(98);
 	}
-	return (0);
-}
-
-/**
- * open_files - Open source and destination files.
- * @source: Path to the source file.
- * @destination: Path to the destination file.
- *
- * Return: 1 if an error occurs during file opening, otherwise 0.
- */
-int open_files(char *source, char *destination)
-{
-	int fd_from, fd_to;
-
-	fd_from = open(source, O_RDONLY);
-	if (fd_from == -1)
+	if (dest_fd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", source);
-		return (1);
-	}
-
-	fd_to = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", destination);
-		close(fd_from);
-		return (1);
-	}
-
-	return (0);
-}
-
-/**
- * copy_content - Copy the content from one file to another.
- * @fd_from: File descriptor of the source file.
- * @fd_to: File descriptor of the destination file.
- */
-void copy_content(int fd_from, int fd_to)
-{
-	ssize_t rd, wr;
-	char buf[BUF_SIZE];
-
-	do {
-		rd = read(fd_from, buf, BUF_SIZE);
-		if (rd == -1)
-		{
-			dprintf(2, "Error: Can't read from file\n");
-			close(fd_from);
-			close(fd_to);
-			exit(98);
-		}
-
-		wr = write(fd_to, buf, rd);
-		if (wr == -1)
-		{
-			dprintf(2, "Error: Can't write to file\n");
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	} while (rd > 0);
-}
-
-/**
- * close_files - Close file descriptors.
- * @fd_from: File descriptor of the source file.
- * @fd_to: File descriptor of the destination file.
- */
-void close_files(int fd_from, int fd_to)
-{
-	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		custom_printf("Error: Can't write to %s\n", args[2]);
+		exit(99);
 	}
 }
 
 /**
- * main - Entry point of the program.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
- *
- * Return: 0 on success, otherwise exit codes based on errors.
+ * main - entry point for the program.
+ * @argc: number of command line arguments.
+ * @argv: array of command line arguments.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
+	int source_fd, dest_fd, close_error;
+	ssize_t num_chars, num_written;
+	char buffer[1024];
 
-	if (check_arguments(argc, argv) == 1)
+	if (argc != 3)
+	{
+		custom_printf("%s\n", "Usage: cp source_file destination_file");
 		exit(97);
+	}
 
-	if (open_files(argv[1], argv[2]) == 1)
-	exit(98);
+	source_fd = custom_open(argv[1], O_RDONLY);
+	dest_fd = custom_open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	check_files(source_fd, dest_fd, argv);
 
-	fd_from = open(argv[1], O_RDONLY);
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	num_chars = 1024;
+	while (num_chars == 1024)
+	{
+		num_chars = custom_read(source_fd, buffer, 1024);
+		if (num_chars == -1)
+			check_files(-1, 0, argv);
+		num_written = custom_write(dest_fd, buffer, num_chars);
+		if (num_written == -1)
+			check_files(0, -1, argv);
+	}
 
-	copy_content(fd_from, fd_to);
-	close_files(fd_from, fd_to);
+	close_error = custom_close(source_fd);
+	if (close_error == -1)
+	{
+		custom_printf("Error: Can't close fd %d\n", source_fd);
+		exit(100);
+	}
+
+	close_error = custom_close(dest_fd);
+	if (close_error == -1)
+	{
+		custom_printf("Error: Can't close fd %d\n", dest_fd);
+		exit(100);
+	}
 
 	return (0);
 }
+
