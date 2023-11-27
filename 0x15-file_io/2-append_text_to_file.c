a03ae1c91e5f08a34e84b5fc8c5eef10bf33a45a
Suvvix <suvvix@gmail.com>
2023-11-27 10:12:42 -0800

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,7 +4,7 @@
  * _strlen - returns the length of a string
  * @str: the string
  *
- * Return: integer length og string
+ * Return: integer length of string
  */
 int _strlen(char *str)
 {
@@ -22,17 +22,27 @@ int _strlen(char *str)
  * @filename: name of file
  * @text_content: text to write
  *
- * Return: 1 on success 0 on failure
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int ox;
-	ssize_t bytes = 0, len = _strlen(text_content);
+	ssize_t bytes = 0, len;
 
 	if (!filename)
 		return (-1);
-	if (len)
+
+	ox = open(filename, O_WRONLY | O_APPEND);
+	if (ox == -1)
+		return (-1);
+
+	len = _strlen(text_content);
+
+	if (len > 0)
 		bytes = write(ox, text_content, len);
+
 	close(ox);
+
 	return (bytes == len ? 1 : -1);
 }
+
