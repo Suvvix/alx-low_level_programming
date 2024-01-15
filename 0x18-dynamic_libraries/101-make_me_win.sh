f628cf0154348367accea9aff3e2f35d3d3c3269
Suvvix <132941647+Suvvix@users.noreply.github.com>
2024-01-15 19:48:50 +0200

    5x

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Suvvix/alx/raw/main/0x18-Dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+#!/biwget -P /tmp https://github.com/Suvvix/alx/raw/main/0x18-Dynamic_libraries/libgiga.so
+wget -P /tmp export LD_PRELOAD=/tmp/libgiga.so
